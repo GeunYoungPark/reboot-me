97a7c4c9db248e21411feeed21438e1457b9a71a
Kelly <GeunYoungPark@users.noreply.github.com>
2026-06-16 10:12:10 +1000

    new post , changed structure

--- blog/blog-data.js
+++ blog/blog-data.js
@@ -9,6 +9,16 @@ const blogPosts = [
 //     link: "posts/pdf-download.html"
 //   },
   {
+    title: "The Party Is Over",
+    category: "Life",
+    date: "2026-06-15",
+    readTime: "1 min read",
+    description: "and the Bill Has Arrived.",
+    keywords: ["Property market"],
+    link: "posts/the-party-is-over-bill-has-arrived.html",
+    lang: "en"
+  },
+  {
     title: "50대, 퇴직 전에 해두면 좋은 것들",
     category: "Life",
     date: "2026-05-25",
@@ -16,7 +26,8 @@ const blogPosts = [
     description: "고정 수입이 사라지기 전에 만들어 두어야 할 작은 안전장치들",
     keywords: ["50대 퇴직 준비", "은퇴 전 준비", "비상금 만들기", "퇴직 후 생활비", "50대 부업", "건강 관리","retirement preparation",
       "emergency fund", "life after 50",  "financial planning",  "side income", "healthy aging"],
-    link: "posts/50s-prepare-retirement.html"
+    link: "posts/50s-prepare-retirement.html",
+    lang: "ko"
   },
   {
     title: "An Easy-to-Remember Full Body Massage Sequence",
@@ -25,7 +36,8 @@ const blogPosts = [
     readTime: "5 min read",
     description: "A simple massage order to help you remember each step from back to front.",
     keywords: ["full body massage","massage sequence","massage routine","massage steps"],
-    link: "posts/simple-massage-sequence.html"
+    link: "posts/simple-massage-sequence.html",
+    lang: "en"
   },
   {
     title: "카테콜아민 반응",
@@ -34,7 +46,8 @@ const blogPosts = [
     readTime: "2 min read",
     description: "내장지방 감량을 이해할 때 알아두면 좋은 핵심 개념",
     keywords: ["카테콜아민 반응","내장지방 감량","스프린트 운동","호르몬 반응","중년 체중 증가"],
-    link: "posts/catecholamine-response-ko.html"
+    link: "posts/catecholamine-response-ko.html",
+    lang: "ko"
   },
   {
     title: "Catecholamine Response",
@@ -43,17 +56,68 @@ const blogPosts = [
     readTime: "2 min read",
     description: "A key concept to understand when thinking about visceral fat loss",
     keywords: ["Catecholamine","Sprint","visceral fat Loss"],
-    link: "posts/catecholamine-response-en.html"
+    link: "posts/catecholamine-response-en.html",
+    lang: "en"
+  },
+   {
+    title: "매일 뭔가 바쁜데, 남는 게 없다",
+    category: "Life",
+    date: "2026-06-02",
+    readTime: "2 min read",
+    description: "계획은 나를 안심시키지만, 결과는 나를 발전시킨다",
+    keywords: ["결과 지향","계획보다 실행","성과 지향","시간 관리","브라이언 트레이시"],
+    link: "posts/busy=but-no-result_ko.html",
+    lang: "ko"
+  },  
+   {
+    title: "아픈 몸으로 번 돈은 정말 이익일까",
+    category: "Wellbeing ",
+    date: "2026-06-03",
+    readTime: "3 min read",
+    description: "50대 이후에는 몸의 회복력을 반드시 고려해야 한다",
+    keywords: ["회복력","일과운동"],
+    link: "posts/hidden-cost-working-through-pain-ko.html",
+    lang: "ko"
   },
   {
-    title: "Unbreakable: book",
-    category: "Wellbeing",
-    date: "2026-05-28",
-    readTime: "1 min read",
-    description: "By Dr vonda Write",
-    keywords: ["aging", "wellbeing", "recovery", "health","unbreakable"],
-    link: "posts/unbreakable.html"
+    title: "The Hidden Cost of Working Through Pain",
+    category: "Wellbeing ",
+    date: "2026-06-03",
+    readTime: "3 min read",
+    description: "After 50, We Must Take the Body’s Recovery Capacity Seriously",
+    keywords: ["resilience","recovery","exercise","work life balanece"],
+    link: "posts/hidden-cost-working-through-pain-en.html",
+    lang: "en"
+  },
+   {
+    title: "나이 들수록 운동은 선택이 아니라 보험이다",
+    category: "Wellbeing ",
+    date: "2026-06-04",
+    readTime: "3 min read",
+    description: "운동은 오늘 기분이 좋아지기 위해서만 하는 것이 아니라, 언젠가 찾아올 몸의 위험을 줄이기 위해 지금부터 쌓아두는 보호 장치다.",
+    keywords: ["노화 운동","medicine3.0","healthspan"],
+    link: "posts/exercise-is-insurance-ko.html",
+    lang: "ko"
+  },
+   {
+    title: "Exercise Is Not a Choice — It Is Insurance",
+    category: "Wellbeing ",
+    date: "2026-06-04",
+    readTime: "3 min read",
+    description: "Exercise is a protection we build now to reduce the physical risks that may come later.",
+    keywords: ["insruance","aging","medicine3.0","healthspan"],
+    link: "posts/exercise-is-insurance-en.html",
+    lang: "en"
   }
+  // {
+  //   title: "Unbreakable: book",
+  //   category: "Wellbeing",
+  //   date: "2026-05-28",
+  //   readTime: "1 min read",
+  //   description: "By Dr vonda Write",
+  //   keywords: ["aging", "wellbeing", "recovery", "health","unbreakable"],
+  //   link: "posts/unbreakable.html"
+  // }
 ];
 
 const suggestedKeywords = [
